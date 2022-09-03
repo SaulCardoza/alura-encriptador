084ac677879032b2d1d6fdd0c7a1e596b13aa2ac
Saul Cardoza <francus1994@gmail.com>
2022-09-03 12:31:41 -0500

    Test boton copiar

--- script.js
+++ script.js
@@ -56,9 +56,9 @@ function dsCript(stringDesencriptada) {
 //botón copiar
 
 function copiarJS() {
-  
-  mensaje.select();
-  navigator.clipboard.writeText(mensaje.value)
+  mensaje.select;
+  mensaje.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(mensaje.value);
   mensaje.value = "";
-  alert("Texto Copiado")
+  alert("Texto copiado al potapapeles");
 }
